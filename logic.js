a490c7e781967412f4c73c0bce75b01ec0f7c6cd
mailDSPublic-dot <mailDSPublic@gmx.de>
2026-05-11 19:30:32 +0200

    umstellen auf unterwebsites

--- logic.js
+++ logic.js
@@ -4,8 +4,10 @@ const open_sidebar = document.getElementById("open_sidebar");
 const close_sidebar = document.getElementById("close_sidebar");
 const html_seite = document.getElementById("html_seite");
 const css_seite = document.getElementById("css_seite");
+const einbindungsmethoden_seite = document.getElementById("einbindungsmethode");
 const start_seite = document.getElementById("start_seite");
 const ausgabe_div = document.getElementById("ausgabe");
+
 
 function sidebar_onclick() {
     // Die Funktion öffnet oder schließt die Sidebar
@@ -17,32 +19,22 @@ function sidebar_onclick() {
         sidebar.style.left = "0px";
         open_sidebar.style.display = "none"
     }
-};
-
-open_sidebar.onclick = sidebar_onclick;
-close_sidebar.onclick = sidebar_onclick;
-
-start_seite.onclick = function () {
-    sidebar_onclick() // Schließt die Sidebar
-    h1.textContent = "HTML und CSS"
-    ausgabe_div.innerHTML = "<p>Hallo auf der Website von Sam und Dan<br>Hier findet ihr einen kurzen Überblick über HTML und CSS</p>"
 }
 
-html_seite.onclick = function () {
+function lade_seite(name) {
     sidebar_onclick() // Schließt die Sidebar
-    h1.textContent = "HTML"
-    ausgabe_div.innerHTML = "<p>Hier kommt dann der HTML Text hin.</p>"
-}
-
-css_seite.onclick = function () {
-    sidebar_onclick() // Schließt die Sidebar
-    h1.textContent = "CSS"
-    fetch("css.html")
+    fetch(`${name}.html`)
         .then(r => r.text())
         .then(html => {
             ausgabe_div.innerHTML = html;
 
-            // Alle Scripts manuell ausführen
+            // CSS einbinden
+            const link = document.createElement("link");
+            link.rel = "stylesheet";
+            link.href = "style_css.css";   // <--- deine CSS-Datei
+            document.head.appendChild(link);
+
+            // Scripts ausführen
             ausgabe_div.querySelectorAll("script").forEach(oldScript => {
                 const newScript = document.createElement("script");
                 newScript.textContent = oldScript.textContent;
@@ -50,6 +42,16 @@ css_seite.onclick = function () {
             });
         });
 }
+open_sidebar.onclick = sidebar_onclick;
+close_sidebar.onclick = sidebar_onclick;
 
+start_seite.onclick = function () {lade_seite("start_seite")}
 
-ausgabe_div.innerHTML = "<p>Hallo auf der Website von Sam und Dan<br>Hier findet ihr einen kurzen Überblick über Html und CSS</p>"
+html_seite.onclick = function () {ausgabe_div.innerHTML = "<p>Hier kommt dann der HTML Text hin.</p>"}
+
+css_seite.onclick = function () { lade_seite("css/css") }
+
+einbindungsmethoden_seite.onclick = function () { lade_seite("css/verbindungsmethoden") }
+
+sidebar_onclick()
+start_seite.onclick()
